13bf8e9fa7f3667f3e6ace7ae0df85ab4e24f108
Abhishek Prajapati <bhishekprajapati@gmail.com>
2024-07-31 19:02:19 +0530

    feat(docs): write docs for next-server-action package

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>✨ Next Server Action</span>,
+  project: {
+    link: "https://github.com/bhishekprajapati/next-server-action",
+  },
+  // ... other theme options
+};
