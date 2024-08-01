602934e24b287c3d497ed8c1087ad6838523ee3f
Abhishek Prajapati <bhishekprajapati@gmail.com>
2024-08-01 13:43:13 +0530

    Change pkg name

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
   logo: <span>✨ Next Server Action</span>,
   project: {
-    link: "https://github.com/bhishekprajapati/next-server-action",
+    link: "https://github.com/bhishekprajapati/next-action-router",
   },
   // ... other theme options
 };
